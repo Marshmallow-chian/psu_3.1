cf2d5837d1c6b9277e4994659286008d8e0414aa
DESKTOP-TTT89NP\Анастасия <n.leksina@bk.ru>
2021-11-03 01:05:18 +0300

    no message

--- main.py
+++ main.py
@@ -97,8 +97,8 @@ try:
         access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)  # 30 min
         access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
         return {"access_token": access_token, "token_type": "bearer"}
-except Exception:
-    print('ошибка')
+except Exception as e:
+    print("----------ERROR", e)
 
 
 # -----------------------------------------------------------------------------------------
