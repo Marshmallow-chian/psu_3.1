71cd04b9abe0e4112cd404e52b4c0b087f0ea1cc
DESKTOP-TTT89NP\Анастасия <n.leksina@bk.ru>
2021-11-03 00:43:32 +0300

    no message

--- main.py
+++ main.py
@@ -71,24 +71,22 @@ async def read_own_items(current_user: UserInDB = Depends(get_current_active_use
 
 # -----------------------------------------------------------------------------------------
 
-try:
-    @app.post("/token", response_model=Token, tags=['token'])
-    async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-        try:
-            user = authenticate_user(form_data.username, form_data.password)  # UserInDB or False
-            if not user:
-                raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail="Incorrect username or password",
-                    headers={"WWW-Authenticate": "Bearer"},
-                )
-            access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)  # 30 min
-            access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
-            return {"access_token": access_token, "token_type": "bearer"}
-        except Exception:
-            '1 ошибка внутри'
-except Exception:
-    '2 ошибка снаружи'
+
+@app.post("/token", response_model=Token, tags=['token'])
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    try:
+        user = authenticate_user(form_data.username, form_data.password)  # UserInDB or False
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)  # 30 min
+        access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+        return {"access_token": access_token, "token_type": "bearer"}
+    except Exception:
+        '1 ошибка внутри'
 # -----------------------------------------------------------------------------------------
 
 
