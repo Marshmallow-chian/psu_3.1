dd10bc5394e8c0b9a554dc23209a3afb4a417829
DESKTOP-TTT89NP\Анастасия <n.leksina@bk.ru>
2021-11-03 00:00:03 +0300

    no message

--- main.py
+++ main.py
@@ -1,107 +1,13 @@
-from datetime import datetime, timedelta
-from typing import Optional, Union, Literal
-import uvicorn
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from passlib.context import CryptContext
-from s_scheme import Token, TokenData
-from scheme import UserInDB, UserOut
-from models import User
-#from dotenv import load_dotenv
-#from pathlib import Path
-#import os
-
-# TODO: add .env
-
-'''load_dotenv()
-env_path = Path('.')/'.env'
-load_dotenv(dotenv_path=env_path)
-
-SECRET_KEY = os.getenv("SECRET_KEY")'''
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-def verify_password(plain_password, hashed_password):  # проверяет правильность пароля, True/False
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):  # хэширует пароль
-    return pwd_context.hash(password)
-
-
-def get_user(user_name: str) -> Union[UserInDB, str]:  # ------------
-    if User.exists(username=user_name):  # если юзер есть в бд, то выводим его
-        user = User.get(username=user_name)
-        return UserInDB.from_orm(user)
-    else:
-        return 'пользователя с таким id не существует'
-
-
-def authenticate_user(username: str, password: str) -> Union[UserInDB, Literal[False]]:  # --------------
-    user = get_user(username)
-    if isinstance(user, str):
-        return False
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):  # ставит метку
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):  # UserInDB
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
 import os.path
 import uvicorn
 from pony.orm import db_session, commit
 from scheme import ProductsOut, ProducerOut, NewProducts, EditProducts, NewProducer, EditProducer, CoolLvL
 from scheme import UserInDB, UserOut, UserEntr
 from models import db, Producer, Products, User
-#from s_main import *
-#from s_scheme import *
-#from datetime import timedelta
-#from fastapi.security import OAuth2PasswordRequestForm
+from s_main import *
+from s_scheme import *
+from datetime import timedelta
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import FastAPI, Body, Depends, status, HTTPException
 
 # использовать exception
@@ -167,7 +73,7 @@ async def read_own_items(current_user: UserInDB = Depends(get_current_active_use
 
 @app.post("/token", response_model=Token, tags=['token'])
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(form_data.username, form_data.password)  # UserInDB or False
+    '''user = authenticate_user(form_data.username, form_data.password)  # UserInDB or False
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -176,7 +82,8 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)  # 30 min
     access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer"}'''
+    return 'ыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыы'
 
 
 # -----------------------------------------------------------------------------------------
